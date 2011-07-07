4565953124a5ce9701f419b7521ea8bb9cffec0b
Daniel Carroll <daniel.c.carroll@gmail.com>
2011-07-06 20:11:37 -0600

    Added Users model (barebones)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,16 @@
 module ApplicationHelper
-    def title
-      base_title = "WDP Announce"
-      if @title.nil?
-        base_title
-      else 
-        "#{base_title} | #{@title}"
-      end
+  
+  def logo
+      image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+     
+  def title
+    base_title = "WDP Announce"
+    if @title.nil?
+      base_title
+    else 
+      "#{base_title} | #{@title}"
     end
+  end
 end 
 
