c26920349fbb39bcf989e29a6cb95691983990f1
Daniel Carroll <daniel.c.carroll@gmail.com>
2011-09-21 16:38:18 -0600

    Finished 10.5 - user update/delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :only => [:edit, :update, :index]
+  before_filter :authenticate, :only => [:edit, :update, :index, :destroy]
   before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user,   :only => :destroy
   
   def new
     @user = User.new
@@ -22,6 +23,12 @@ class UsersController < ApplicationController
       @title = "Sign up"
       render 'new'
     end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
   end
   
   def edit
@@ -55,4 +62,7 @@ class UsersController < ApplicationController
       redirect_to(root_path) unless current_user?(@user)
     end
     
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 end
